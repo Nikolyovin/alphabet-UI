e9f2b8f7c6a9c311683a21ddddd9d541e43cd37e
Nikolyovin <pulya0763@gmail.com>
2023-03-02 23:33:09 +0300

    fix navigatiom

--- src/screens/StartScreen.tsx
+++ src/screens/StartScreen.tsx
@@ -1,26 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
-const StartScreen = ({ navigation }) => {
-  return (
-    <View style={styles.wrap}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('Home')}
-      >
-        <Text style={styles.textButton}>Start</Text>
-      </TouchableOpacity>
-    </View>
-  )
+const StartScreen = ({ navigation }: any) => {
+	return (
+		<View style={styles.wrap}>
+			<TouchableOpacity
+				style={styles.button}
+				onPress={() => navigation.navigate('Home')}
+				// onPress={() => console.log('efwfwfwef')}
+			>
+				<Text style={styles.textButton}>Start</Text>
+			</TouchableOpacity>
+		</View>
+	)
 }
 
 export default StartScreen
 
 const styles = StyleSheet.create({
-  wrap: {
-    backgroundColor: '#64b5f6',
-  },
-  button: {},
-  textButton: {},
+	wrap: {
+		// backgroundColor: '#64b5f6',
+		height: 500,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	button: {
+		backgroundColor: '#64b5f6',
+		width: 100,
+		height: 30,
+	},
+	textButton: {},
 })
